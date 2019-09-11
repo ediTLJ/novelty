652212ea3335d1e1ae5e4eff235b301ed125c272
Eduard Scarlat <edi.tlj@gmail.com>
2019-09-11 19:08:07 +0300

    added missing license info

--- app/src/main/java/ro/edi/novelty/data/remote/RssItem.kt
+++ app/src/main/java/ro/edi/novelty/data/remote/RssItem.kt
@@ -1,3 +1,18 @@
+/*
+* Copyright 2019 Eduard Scarlat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package ro.edi.novelty.data.remote
 
 import org.simpleframework.xml.Element
@@ -6,7 +21,6 @@ import org.simpleframework.xml.Root
 import java.io.Serializable
 
 @Root(name = "item", strict = false)
-@Namespace(prefix = "dc", reference = "http://purl.org/dc/elements/1.1/")
 data class RssItem(
     @field:Element(name = "title", required = false)
     @param:Element(name = "title", required = false)
@@ -20,6 +34,7 @@ data class RssItem(
     @param:Element(name = "description", required = false)
     val description: String? = null,
 
+    @Namespace(prefix = "dc", reference = "http://purl.org/dc/elements/1.1/")
     @field:Element(name = "creator", required = false)
     @param:Element(name = "creator", required = false)
     val author: String? = null,
