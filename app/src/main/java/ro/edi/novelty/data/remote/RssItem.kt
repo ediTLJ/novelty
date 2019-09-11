12bb9441d6dd72efe107e247fbb93b8eda419c92
Eduard Scarlat <edi.tlj@gmail.com>
2019-09-11 16:55:56 +0300

    parse RSS item author (via dc:creator)

--- app/src/main/java/ro/edi/novelty/data/remote/RssItem.kt
+++ app/src/main/java/ro/edi/novelty/data/remote/RssItem.kt
@@ -1,10 +1,12 @@
 package ro.edi.novelty.data.remote
 
 import org.simpleframework.xml.Element
+import org.simpleframework.xml.Namespace
 import org.simpleframework.xml.Root
 import java.io.Serializable
 
 @Root(name = "item", strict = false)
+@Namespace(prefix = "dc", reference = "http://purl.org/dc/elements/1.1/")
 data class RssItem(
     @field:Element(name = "title", required = false)
     @param:Element(name = "title", required = false)
@@ -18,8 +20,8 @@ data class RssItem(
     @param:Element(name = "description", required = false)
     val description: String? = null,
 
-    @field:Element(name = "author", required = false)
-    @param:Element(name = "author", required = false)
+    @field:Element(name = "creator", required = false)
+    @param:Element(name = "creator", required = false)
     val author: String? = null,
 
     @field:Element(name = "id", required = false)
